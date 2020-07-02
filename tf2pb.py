11d5c73e438d29c3475d0cc287b81e76c76ff22e
NaN <52284359+IAMAl@users.noreply.github.com>
2020-07-02 18:58:14 +0900

    Add files via upload
    tf_utils.py", line 116, in map_tf_dtype
    dtype = TF_TO_ONNX_DTYPE[dtype]
    KeyError: tf.float32_ref

--- tf2pb.py
+++ tf2pb.py
@@ -1,6 +1,13 @@
+import os
+
 from absl import app 
 from absl import flags
+
 import tensorflow as tf
+import util
+
+import onnx
+import tf2onnx
 
 '''Import Model from model.py file'''
 import model
@@ -9,6 +16,8 @@ flags.DEFINE_integer('batch_size',   4, 'The size of a sample batch.')
 flags.DEFINE_integer('img_height', 128, 'Image height.')
 flags.DEFINE_integer('img_width',  416, 'Image width.')
 flags.DEFINE_integer('seq_length',   3, 'Sequence length for each example.')
+
+flags.DEFINE_string('model_ckpt', None, 'Model checkpoint to load.')
 flags.DEFINE_string('output_path', "output_path", 'Output File Name')
 flags.DEFINE_string('output_file_name', "model.pb", 'Output File Name')
 FLAGS = flags.FLAGS
@@ -18,21 +27,43 @@ def _run_pb_gen():
 
   '''Load Model from model.py file''' 
   inference_model = model.Model(is_training=False,
-                                seq_length=FLAGS.seq_length,
-                                batch_size=FLAGS.batch_size,
-                                img_height=FLAGS.img_height,
-                                img_width=FLAGS.img_width)
+                seq_length=FLAGS.seq_length,
+                batch_size=FLAGS.batch_size,
+                img_height=FLAGS.img_height,
+                img_width=FLAGS.img_width)
 
-  '''Make Session'''
-  sess = tf.Session()
+  with tf.Session() as sess:
+    '''Initialize Variables in Model'''
+    sess.run(tf.compat.v1.global_variables_initializer())
 
-  '''Initialize Variables in Model'''
-  sess.run(tf.global_variables_initializer())
+    '''Get Graph Def'''
+    graph = sess.graph
 
-  '''Store pb-file'''
-  tf.io.write_graph(sess.graph.as_graph_def(), str(FLAGS.output_path), str(FLAGS.output_file_name), as_text=False)
+    '''Extract Inputs'''
+    inputs = []
+    for op in sess.graph.get_operations():
+        if op.type == "Placeholder":
+          inputs.append(op.name)
 
-  print('TF-Graph converted to SavedModel!')
+    '''Extract Outputs'''
+    name_list = []
+    exclsv_list = []
+    for node in graph.as_graph_def().node:
+        name_list.append(node.name)
+        exclsv_list.extend(node.input)
+    outputs = set(name_list) - set(exclsv_list)
+
+    g = tf2onnx.tfonnx.process_tf_graph(sess.graph, input_names=inputs, output_names=outputs)
+
+    '''Make ProtoBuff Model'''
+    model_proto = g.make_model(str(FLAGS.output_path))
+    onnx.checker.check_model(model_proto)
+
+    '''Store pb-file'''
+    tf2onnx.utils.save_onnx_model("./", "saved_model", feed_dict={}, model_proto=model_proto)
+    #tf.io.write_graph(graph, str(FLAGS.output_path), str(FLAGS.output_file_name), as_text=False)
+
+    print('TF-Graph converted to SavedModel!')
 
 
 def main(_):
