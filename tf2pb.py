11444b3574d5ed23ae66075c3e9a14bdf880f92d
IAMAl <52284359+IAMAl@users.noreply.github.com>
2020-07-03 12:05:44 +0900

    update: decice setting in freeze_session

--- tf2pb.py
+++ tf2pb.py
@@ -27,14 +27,14 @@ def freeze_session(session, keep_var_names=None, output_names=None, clear_device
 
     graph = session.graph
     with graph.as_default():
-        freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()).difference(keep_var_names or []))
-        output_names = output_names or []
-        output_names += [v.op.name for v in tf.compat.v1.global_variables()]
-        # Graph -> GraphDef ProtoBuf
-        input_graph_def = graph.as_graph_def()
-        if clear_devices:
-            for node in input_graph_def.node:
-                node.device = ""
+      freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()).difference(keep_var_names or []))
+      output_names = output_names or []
+      output_names += [v.op.name for v in tf.compat.v1.global_variables()]
+      # Graph -> GraphDef ProtoBuf
+      input_graph_def = graph.as_graph_def()
+      if clear_devices:
+        for index, _ in enumerate(input_graph_def.node):
+          input_graph_def.node[index].device = ""
         frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(session, input_graph_def, output_names, freeze_var_names)
         return frozen_graph
 
